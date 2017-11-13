8eadb709486e7cf088428134866520ec5364edd9
Benoît Fleury <me@benfle.com>
2017-11-13 21:52:40 +0100

    add file input

---
+++ input/file.js
@@ -0,0 +1,39 @@
+const Tail = require('tail').Tail
+const nginx = require('../format/nginx.js')
+
+function create ({path, parse = nginx.parser()} = {}) {
+  return {
+    name: 'File',
+    start: (pipeline) => {
+      let tail
+      try {
+        tail = new Tail(path, {
+          logger: {
+            info: msg => {},
+            error: err => pipeline.status(err, err.message)
+          }
+        })
+      } catch (err) {
+        return pipeline.status(err, err.message)
+      }
+      tail.on('line', (data) => {
+        let log
+        try {
+          log = parse(data)
+        } catch (err) {
+          return pipeline.error(err)
+        }
+        pipeline.success(log)
+      })
+      tail.on('error', (err) => {
+        pipeline.error(err)
+      })
+      tail.watch()
+      pipeline.status(null, 'Watching ' + path)
+    }
+  }
+}
+
+module.exports = {
+  create: create
+}
