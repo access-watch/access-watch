51fe0dfaf6ec6863e58061ac9d8bd5bab712692f
jduthon <duthon.jean@gmail.com>
2017-11-16 16:02:19 +0100

    Removed batch in websocket

--- input/websocket.js
+++ input/websocket.js
@@ -9,18 +9,14 @@ function create ({address, parse}) {
       socket.on('open', _ => {
         pipeline.status(null, 'Listening to ' + address)
       })
-      socket.on('message', stringMessage => {
-        const json = JSON.parse(stringMessage)
-        const messages = Array.isArray(json) ? json : [json]
-        messages.forEach(message => {
-          let log
-          try {
-            log = parse(message)
-          } catch (err) {
-            return pipeline.error(err)
-          }
-          pipeline.success(log)
-        })
+      socket.on('message', message => {
+        let log
+        try {
+          log = parse(JSON.parse(message))
+        } catch (err) {
+          return pipeline.error(err)
+        }
+        pipeline.success(log)
       })
       socket.on('error', err => {
         pipeline.status(err, 'Websocket error')
