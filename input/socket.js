120bc32c3cd14ff0d77708a63caee4a259ec81c6
François Hodierne <francois@hodierne.net>
2017-12-10 17:13:46 +0100

    create a socket input and base syslog on it

---
+++ input/socket.js
@@ -0,0 +1,70 @@
+const dgram = require('dgram')
+const net = require('net')
+
+const { fromJS } = require('immutable')
+
+const defaultParse = s => fromJS(JSON.parse(s))
+
+function createTcpServer ({pipeline, name, port, handler}) {
+  return net
+    .createServer(socket => {
+      socket.on('data', data => {
+        data.toString().split('\n').forEach(line => {
+          if (line) {
+            handler(line)
+          }
+        })
+      })
+    })
+    .listen(port, err => {
+      if (err) {
+        pipeline.status(err, 'Cannot start: ' + err.message)
+      } else {
+        pipeline.status(null, `Listening on port ${port}.`)
+        console.log(`${name} listening on TCP port ${port}.`)
+      }
+    })
+}
+
+function createUdpServer ({pipeline, name, port, handler}) {
+  return dgram
+    .createSocket('udp4')
+    .on('message', message => {
+      handler(message.toString())
+    })
+    .on('error', err => {
+      pipeline.status(err, 'Cannot start: ' + err.message)
+    })
+    .on('listening', () => {
+      pipeline.status(null, `Listening on port ${port}.`)
+      console.log(`${name} listening on UDP port ${port}.`)
+    })
+    .bind(port)
+}
+
+function create ({name = 'Socket', protocol, port, parse = defaultParse}) {
+  return {
+    name: name,
+    start: pipeline => {
+      const handler = message => {
+        try {
+          pipeline.success(parse(message))
+        } catch (err) {
+          pipeline.error(err)
+        }
+      }
+      if (!protocol || protocol === 'udp') {
+        createUdpServer({pipeline, name, port, handler})
+      }
+      if (!protocol || protocol === 'tcp') {
+        createTcpServer({pipeline, name, port, handler})
+      }
+    }
+  }
+}
+
+module.exports = {
+  createTcpServer,
+  createUdpServer,
+  create
+}
--- input/syslog.js
+++ input/syslog.js
@@ -1,56 +1,8 @@
-const net = require('net')
-const dgram = require('dgram')
-
 const syslogParse = require('syslog-parse')
 
 const { fromJS } = require('immutable')
 
-function createTcpServer ({pipeline, name, port, parse}) {
-  return net
-    .createServer(socket => {
-      socket.on('data', data => {
-        data.toString().split('\n').forEach(line => {
-          if (line) {
-            try {
-              const result = syslogParse(line)
-              pipeline.success(parse(result.message))
-            } catch (err) {
-              pipeline.error(err)
-            }
-          }
-        })
-      })
-    })
-    .listen(port, err => {
-      if (err) {
-        pipeline.status(err, 'Cannot start: ' + err.message)
-      } else {
-        pipeline.status(null, `Listening on port ${port}.`)
-        console.log(`${name} listening on TCP port ${port}.`)
-      }
-    })
-}
-
-function createUdpServer ({pipeline, name, port, parse}) {
-  return dgram
-    .createSocket('udp4')
-    .on('message', message => {
-      try {
-        const result = syslogParse(message.toString())
-        pipeline.success(parse(result.message))
-      } catch (err) {
-        pipeline.error(err)
-      }
-    })
-    .on('error', err => {
-      pipeline.status(err, 'Cannot start: ' + err.message)
-    })
-    .on('listening', () => {
-      pipeline.status(null, `Listening on port ${port}.`)
-      console.log(`${name} listening on UDP port ${port}.`)
-    })
-    .bind(port)
-}
+const socket = require('./socket')
 
 const defaultParse = s => fromJS(JSON.parse(s))
 
@@ -58,11 +10,19 @@ function create ({name = 'Syslog', protocol, port = 514, parse = defaultParse}) 
   return {
     name: name,
     start: pipeline => {
+      const handler = message => {
+        try {
+          const result = syslogParse(message)
+          pipeline.success(parse(result.message))
+        } catch (err) {
+          pipeline.error(err)
+        }
+      }
       if (!protocol || protocol === 'udp') {
-        createUdpServer({pipeline, name, port, parse})
+        socket.createUdpServer({pipeline, name, port, handler})
       }
       if (!protocol || protocol === 'tcp') {
-        createTcpServer({pipeline, name, port, parse})
+        socket.createTcpServer({pipeline, name, port, handler})
       }
     }
   }
