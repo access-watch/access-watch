f53bd8b26bcd19101fcb31ff7023e91bd4fa4ab6
jduthon <duthon.jean@gmail.com>
2018-03-01 10:22:50 +0100

    Refactoring: FilterGroup => Search

--- src/databases.js
+++ src/databases.js
@@ -2,7 +2,7 @@ const databases = {
   metrics: require('./lib/metrics').connect(),
   session: require('./lib/session').connect(),
   rules: require('./lib/rules').connect(),
-  filterGroups: require('./lib/filter-groups').connect(),
+  searches: require('./lib/searches').connect(),
 };
 
 module.exports = databases;
