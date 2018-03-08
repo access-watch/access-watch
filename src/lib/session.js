fd3574df0f21dac762b1d58a1f6dce376de0ea32
François Hodierne <francois@hodierne.net>
2018-03-08 14:15:44 +0100

    instruments

--- src/lib/session.js
+++ src/lib/session.js
@@ -7,6 +7,7 @@ const { now } = require('./util');
 const { Speed } = require('./speed');
 const database = require('./database');
 const config = require('../constants');
+const instruments = require('./instruments');
 
 function withSpeed(session) {
   return session
@@ -22,6 +23,12 @@ class Database {
   constructor() {
     this.sessions = Map();
     this.indexes = Map();
+
+    setInterval(() => {
+      this.sessions.entrySeq().forEach(([key, value]) => {
+        instruments.gauge(`sessions.${key}.size`, value.size);
+      });
+    }, 1000);
   }
 
   gc() {
